8713d3b76ca2e8a33d3ef76d2807c5c99d0bc947
Naman Gera <namangera15@gmail.com>
2022-12-19 13:20:59 +0000

    Update the examples for the users

--- examples/normalize-data.py
+++ examples/normalize-data.py
@@ -1,31 +1,17 @@
-import cupy
-from mpi4py import MPI
+import cupy as cp
+import numpy as np
 
 from httomolib.normalisation import normalize_cupy
-from loaders import standard_tomo
-
-comm = MPI.COMM_WORLD
-
-# Define input file and relevant internal NeXuS paths
-in_file = 'data/tomo_standard.nxs'
-data_key = '/entry1/tomo_entry/data/data'
-image_key = '/entry1/tomo_entry/data/image_key'
-DIMENSION = 1
-PREVIEW = [None, None, None]
-PAD = 0
 
 # Load the projection data
-(   host_data,
-    host_flats,
-    host_darks,
-    angles_radians,
-    angles_total,
-    detector_y,
-    detector_x,
-) = standard_tomo(in_file, data_key, image_key, DIMENSION, PREVIEW, PAD, comm)
+in_file = 'data/tomo_standard.npz'
+datafile = np.load(in_file)
+host_data = datafile['data']
+host_flats = datafile['flats']
+host_darks = datafile['darks']
 
 # Apply CuPy implementation of normalisation to data
-data = cupy.asarray(host_data)
-flats = cupy.asarray(host_flats)
-darks = cupy.asarray(host_darks)
+data = cp.asarray(host_data)
+flats = cp.asarray(host_flats)
+darks = cp.asarray(host_darks)
 data = normalize_cupy(data, flats, darks)
--- examples/paganin-filter.py
+++ examples/paganin-filter.py
@@ -1,22 +1,12 @@
 import cupy as cp
-from mpi4py import MPI
+import numpy as np
 
 from httomolib.filtering import paganin_filter
-from loaders import standard_tomo
 
-comm = MPI.COMM_WORLD
-
-# Define input file and relevant internal NeXuS paths
-in_file = 'data/tomo_standard.nxs'
-data_key = '/entry1/tomo_entry/data/data'
-image_key = '/entry1/tomo_entry/data/image_key'
-DIMENSION = 1
-PREVIEW = [None, None, None]
-PAD = 0
-
-# Load the projection data
-host_data = \
-    standard_tomo(in_file, data_key, image_key, DIMENSION, PREVIEW, PAD, comm)[0]
+# Load data
+in_file = 'data/tomo_standard.npz'
+datafile = np.load(in_file)
+host_data = datafile['data']
 data = cp.asarray(host_data)
 
 # Apply CuPy implementation of Paganin filter to projection data
