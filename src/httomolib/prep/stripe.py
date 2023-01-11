c367203a19647e5d2c30fc9e6b56a8d260a2ec4b
Naman Gera <namangera15@gmail.com>
2023-01-11 17:30:00 +0000

    Improve the docstrings

--- src/httomolib/prep/stripe.py
+++ src/httomolib/prep/stripe.py
@@ -26,17 +26,23 @@ from cupy import abs, mean, ndarray
 from cupyx.scipy.ndimage import median_filter
 
 __all__ = [
+    'detect_stripes',
+    'merge_stripes',
     'remove_stripe_based_sorting_cupy',
-    'remove_stripes_titarenko_cupy',    
+    'remove_stripes_titarenko_cupy',
 ]
+
 # TODO: port 'remove_all_stripe', 'remove_large_stripe' and 'remove_dead_stripe'
 # from https://github.com/tomopy/tomopy/blob/master/source/tomopy/prep/stripe.py
 
-## %%%%%%%%%%%%%%%%%remove_stripe_based_sorting_cupy%%%%%%%%%%%%%%%%%%%%%  ##
-# Naive CuPy port of the NumPy implementation in TomoPy
-def remove_stripe_based_sorting_cupy(tomo: ndarray,
-                                     size: int = None,
-                                     dim: int = 1) -> ndarray:
+
+## %%%%%%%%%%%%%%%%% remove_stripe_based_sorting_cupy %%%%%%%%%%%%%%%%%%%%%  ##
+## Naive CuPy port of the NumPy implementation in TomoPy
+def remove_stripe_based_sorting_cupy(
+    tomo: ndarray,
+    size: int = None,
+    dim: int = 1
+) -> ndarray:
     """
     Remove full and partial stripe artifacts from sinogram using Nghia Vo's
     approach :cite:`Vo:18` (algorithm 3).
@@ -46,7 +52,7 @@ def remove_stripe_based_sorting_cupy(tomo: ndarray,
     ----------
     tomo : cupy.ndarray
         3D tomographic data.
-    size : int
+    size : int, optional
         Window size of the median filter.
     dim : {1, 2}, optional
         Dimension of the window.
@@ -56,16 +62,20 @@ def remove_stripe_based_sorting_cupy(tomo: ndarray,
     cupy.ndarray
         Corrected 3D tomographic data.
     """
+
     matindex = _create_matindex(tomo.shape[2], tomo.shape[0])
     if size is None:
         if tomo.shape[2] > 2000:
             size = 21
         else:
             size = max(5, int(0.01 * tomo.shape[2]))
+
     for m in range(tomo.shape[1]):
         sino = tomo[:, m, :]
         tomo[:, m, :] = _rs_sort(sino, size, matindex, dim)
+
     return tomo
+
 
 def _create_matindex(nrow, ncol):
     """
@@ -92,27 +102,30 @@ def _rs_sort(sinogram, size, matindex, dim):
         [row[row[:, 0].argsort()] for row in matsort])
     sino_corrected = matsortback[:, :, 1]
     return cp.transpose(sino_corrected)
-    
-## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
 
-## %%%%%%%%%%%%%%%%%%%remove_stripes_titarenko_cupy%%%%%%%%%%%%%%%%%%%%%%%  ##
-def remove_stripes_titarenko_cupy(data: ndarray,
-                                  beta: float = 0.1) -> ndarray:
+
+## %%%%%%%%%%%%%%%%%%% remove_stripes_titarenko_cupy %%%%%%%%%%%%%%%%%%%%%%%  ##
+def remove_stripes_titarenko_cupy(
+    data: ndarray,
+    beta: float = 0.1
+) -> np.ndarray:
     """
     Removes stripes with the method of V. Titarenko (TomoCuPy implementation)
 
     Parameters
     ----------
-    arr : ndarray
+    data : ndarray
         3D stack of projections as a CuPy array.
     beta : float, optional
         filter parameter, lower values increase the filter strength.
+        Default is 0.1
 
     Returns
     -------
     ndarray
         3D CuPy array of de-striped projections.
     """
+
     gamma = beta * ((1 - beta) / (1 + beta)) ** abs(
         cp.fft.fftfreq(data.shape[-1]) * data.shape[-1]
     )
@@ -123,72 +136,104 @@ def remove_stripes_titarenko_cupy(data: ndarray,
     data[:] += v
 
     return data
-## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
 
 
-## %%%%%%%%%%%%%%%%%%%%%%%%detect_stripes%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
-def detect_stripes(data: ndarray,
-                   search_window_dims: tuple = (1,9,1),
-                   vert_window_size: float = 5,
-                   gradient_gap: int = 2,
-                   ncore: int = 1) -> ndarray: 
-    """module to detect stripes in sinograms (2D) OR projection data (3D). 
-    1. Taking first derrivative of the input in the direction orthogonal to stripes.
-    2. Slide horizontal rectangular window orthogonal to stripes direction to accenuate outliers (stripes) using median.
-    3. Slide the vertical thin (1 pixel) window to calculate a mean (further accenuates stripes).
+## %%%%%%%%%%%%%%%%%%%%%%%% detect_stripes %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
+def detect_stripes(
+    data: np.ndarray,
+    search_window_dims: tuple = (1, 9, 1),
+    vert_window_size: float = 5.,
+    gradient_gap: int = 2,
+    ncore: int = 1
+) -> np.ndarray:
+    """
+    Detects stripes in sinograms (2D) or projection data (3D).
 
-    Args:
-        data (ndarray): sinogram (2D) [angles x detectorsX] OR projection data (3D) [angles x detectorsY x detectorsX]
-        search_window_dims (tuple, optional): searching rectangular window for weights calculation, 
-        of a size (detectors_window_height, detectors_window_width, angles_window_depth). Defaults to (1,9,1).
-        vert_window_size (float, optional): the half size of the vertical 1D window to calculate mean. Given in percents relative to the size of the angle dimension. Defaults to 5.
-        gradient_gap (int, optional):  the gap in pixels with the neighbour while calculating a gradient (1 is the normal gradient). Defaults to 2.
-        ncore (int, optional): _description_. Defaults to 1.
+    The algorithm is based on the following steps:
 
-    Returns:
-        ndarray: The associated weights (needed for thresholding)
+    - Take first derivative of the input in the direction orthogonal to the stripes.
+    - Slide horizontal rectangular window orthogonal to stripes
+        direction to accentuate outliers (stripes) using median.
+    - Slide the vertical thin (1 pixel) window to calculate a
+        mean (further accentuates stripes).
+
+    Parameters
+    ----------
+    data : np.ndarray
+        2D Sinogram (2D) [angles x detectorsX] OR
+        projection data (3D) [angles x detectorsY x detectorsX]
+    search_window_dims : tuple, optional
+        Searching rectangular window for weights calculation,
+        of a size (detectors_window_height, detectors_window_width, angles_window_depth).
+        Defaults to (1, 9, 1).
+    vert_window_size : float, optional
+        The half size of the vertical 1D window to calculate mean.
+        Given in percents relative to the size of the angle dimension. Defaults to 5.
+    gradient_gap : int, optional
+        The gap in pixels with the neighbour while calculating a gradient
+        (1 is the normal gradient). Defaults to 2.
+    ncore : int, optional
+        The number of CPU cores to use. Defaults to 1.
+
+    Returns
+    -------
+    np.ndarray
+        The associated weights (needed for thresholding)
     """       
     from larix.methods.misc import STRIPES_DETECT
 
     # calculate weights for stripes
-    (stripe_weights, stats_vec) = STRIPES_DETECT(data, search_window_dims, vert_window_size, gradient_gap, ncore)
-       
+    stripe_weights = STRIPES_DETECT(data,
+        search_window_dims, vert_window_size, gradient_gap, ncore)[0]
+
     return stripe_weights
-## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
 
-## %%%%%%%%%%%%%%%%%%%%%%%%merge_stripes%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
-def merge_stripes(data: ndarray,
-                   stripe_width_max_perc: float = 5,
-                   mask_dilate: int = 2,
-                   threshold_stripes: float = 0.1,
-                   ncore: int = 1) -> ndarray:
-    """module to threshold the obtained stripe weights in sinograms (2D) OR projection data (3D) and merge stripes that are close to each other. 
+
+## %%%%%%%%%%%%%%%%%%%%%%%% merge_stripes %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
+def merge_stripes(
+    data: np.ndarray,
+    stripe_width_max_perc: float = 5.,
+    mask_dilate: int = 2,
+    threshold_stripes: float = 0.1,
+    ncore: int = 1
+) -> np.ndarray:
+    """
+    Thresholds the obtained stripe weights in 2D sinograms OR 3D projection data
+    and merge stripes that are close to each other.
     
-    Args:
-        data (ndarray): weigths for sinogram (2D) [angles x detectorsX] OR projection data (3D) [angles x detectorsY x detectorsX]
-        stripe_width_max_perc (float, optional):  the maximum width of stripes in the data, given in percents relative to the size of the DetectorX. Defaults to 5.
-        mask_dilate (int, optional): the number of pixels/voxels to dilate the obtained mask. Defaults to 2.
-        threshold_stripes (float, optional): Threshold the obtained weights to get a binary mask, larger vaules are more sensitive to stripes. Defaults to 0.1.
-        ncore (int, optional): _description_. Defaults to 1.
+    Parameters
+    ----------
+    data : np.ndarray
+        Weights for 2D sinogram [angles x detectorsX] OR
+        3D projection data [angles x detectorsY x detectorsX]
+    stripe_width_max_perc : float, optional
+        The maximum width of stripes in the data,
+        given in percents relative to the size of the DetectorX.
+        Defaults to 5.
+    mask_dilate : int, optional
+        The number of pixels/voxels to dilate the obtained mask.
+        Defaults to 2.
+    threshold_stripes : float, optional
+        Threshold the obtained weights to get a binary mask,
+        larger vaules are more sensitive to stripes. Defaults to 0.1
+    ncore : int, optional
+        The number of CPU cores to use. Defaults to 1.
 
-    Returns:
-        ndarray: mask_stripe
+    Returns
+    -------
+    np.ndarray
+        Returns the numpy array with the stripes merged.
     """       
     from larix.methods.misc import STRIPES_MERGE
     
-    gradientX = _gradient(data, 2)    
+    gradientX = np.gradient(data, axis=2)
     med_val = np.median(np.abs(gradientX).flatten(), axis=0)
     
     # we get a local stats here, needs to be adopted for global stats
-    mask_stripe = np.zeros_like(data,dtype="uint8")    
+    mask_stripe = np.zeros_like(data, dtype="uint8")
     mask_stripe[data > med_val/threshold_stripes] = 1
     
     # merge stripes that are close to each other
     mask_stripe_merged = STRIPES_MERGE(mask_stripe, stripe_width_max_perc, mask_dilate, ncore)
     
     return mask_stripe_merged
-
-
-def _gradient(data, axis):
-    return np.gradient(data, axis=axis)
-## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  ##
