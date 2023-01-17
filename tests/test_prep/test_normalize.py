e2b394c36c31146143978e618ee7ac8137d2f3b0
Naman Gera <namangera15@gmail.com>
2023-01-17 14:20:51 +0000

    Fix whitespace

--- tests/test_prep/test_normalize.py
+++ tests/test_prep/test_normalize.py
@@ -15,7 +15,7 @@ def test_normalize():
     host_data = datafile['data']
     host_flats = datafile['flats']
     host_darks = datafile['darks']
-    
+
     data = cp.asarray(host_data)
     flats = cp.asarray(host_flats)
     darks = cp.asarray(host_darks)
@@ -34,11 +34,11 @@ def test_normalize():
     for _ in range(10):
         assert_allclose(cp.min(data_normalize_cupy), data_min, rtol=1e-05)
         assert_allclose(cp.max(data_normalize_cupy), data_max, rtol=1e-05)
-    
+
     #--- testing normalize_raw_cuda  ---#
     # TODO: fix the normalize_raw_cuda test failing at max assert
-    #data_normalize_raw_cuda = normalize_raw_cuda(data, flats, darks)
-    #for _ in range(10):
+    # data_normalize_raw_cuda = normalize_raw_cuda(data, flats, darks)
+    # for _ in range(10):
     #    assert_allclose(cp.min(data_normalize_raw_cuda), data_min, rtol=1e-05)
     #    assert_allclose(cp.max(data_normalize_raw_cuda), data_max, rtol=1e-05)
 
