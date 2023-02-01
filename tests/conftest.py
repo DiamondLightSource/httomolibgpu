2def661abfa8c65d2e3b9edee208a4ef39e0fa0a
Team GPU <team-gpu@xceleit.com>
2023-02-01 12:36:10 +0000

    Refactors alignment and normalize tests

---
+++ tests/conftest.py
@@ -0,0 +1,104 @@
+# Defines common fixtures and makes them available to all tests
+
+import pytest
+import os
+import numpy as np
+import cupy as cp
+
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+@pytest.fixture(scope="session")
+def test_data_path():
+    return os.path.join(CUR_DIR, "test_data")
+
+
+@pytest.fixture(scope="session")
+def distortion_correction_path(test_data_path):
+    return os.path.join(test_data_path, "distortion-correction")
+
+
+# only load from disk once per session, and we use np.copy for the elements,
+# to ensure data in this loaded file stays as originally loaded
+@pytest.fixture(scope="session")
+def data_file(test_data_path):
+    in_file = os.path.join(test_data_path, "tomo_standard.npz")
+    return np.load(in_file)
+
+
+@pytest.fixture
+def ensure_clean_memory():
+    cp.get_default_memory_pool().free_all_blocks()
+    return None
+
+
+@pytest.fixture
+def host_data(data_file):
+    return np.copy(data_file["data"])
+
+
+@pytest.fixture
+def data(host_data, ensure_clean_memory):
+    return cp.asarray(host_data)
+
+
+@pytest.fixture
+def host_flats(data_file):
+    return np.copy(data_file["flats"])
+
+
+@pytest.fixture
+def flats(host_flats, ensure_clean_memory):
+    return cp.asarray(host_flats)
+
+
+@pytest.fixture
+def host_darks(
+    data_file,
+):
+    return np.copy(data_file["darks"])
+
+
+@pytest.fixture
+def darks(host_darks, ensure_clean_memory):
+    return cp.asarray(host_darks)
+
+
+@pytest.fixture
+def host_angles(data_file):
+    return np.copy(data_file["angles"])
+
+
+@pytest.fixture
+def angles(host_angles, ensure_clean_memory):
+    return cp.asarray(host_angles)
+
+
+@pytest.fixture
+def host_angles_total(data_file):
+    return np.copy(data_file["angles_total"])
+
+
+@pytest.fixture
+def angles_total(host_angles_total, ensure_clean_memory):
+    return cp.asarray(host_angles_total)
+
+
+@pytest.fixture
+def host_detector_y(data_file):
+    return np.copy(data_file["detector_y"])
+
+
+@pytest.fixture
+def detector_y(host_detector_y, ensure_clean_memory):
+    return cp.asarray(host_detector_y)
+
+
+@pytest.fixture
+def host_detector_x(data_file):
+    return np.copy(data_file["detector_x"])
+
+
+@pytest.fixture
+def detector_x(host_detector_x, ensure_clean_memory):
+    return cp.asarray(host_detector_x)
--- tests/test_prep/test_normalize.py
+++ tests/test_prep/test_normalize.py
@@ -1,41 +1,32 @@
 import cupy as cp
 import numpy as np
 import pytest
-from cupy.testing import assert_allclose
+from numpy.testing import assert_allclose
 
 from httomolib.prep.normalize import normalize_cupy
 
 
 @cp.testing.gpu
-def test_normalize():
-    # testing cupy implementation for normalization
-
-    in_file = 'tests/test_data/tomo_standard.npz'
-    datafile = np.load(in_file)
-    # keys: data, flats, darks, angles, angles_total, detector_y, detector_x
-    host_data = datafile['data']
-    host_flats = datafile['flats']
-    host_darks = datafile['darks']
-
-    data = cp.asarray(host_data)
-    flats = cp.asarray(host_flats)
-    darks = cp.asarray(host_darks)
-
-    data_min = cp.array(-0.16163824, dtype=cp.float32)
-    data_max = cp.array(2.7530956, dtype=cp.float32)
-
+def test_normalize_1D_raises(data, flats, darks):
     _data_1d = cp.ones(10)
+
     #: data cannot be a 1D array
-    pytest.raises(ValueError, lambda: normalize_cupy(_data_1d, flats, darks))
+    with pytest.raises(ValueError):
+        normalize_cupy(_data_1d, flats, darks)
+
     #: flats cannot be a 1D array
-    pytest.raises(ValueError, lambda: normalize_cupy(data, _data_1d, darks))
+    with pytest.raises(ValueError):
+        normalize_cupy(data, _data_1d, darks)
+
+
+@cp.testing.gpu
+def test_normalize(data, flats, darks):
+    # testing cupy implementation for normalization
+    data_min = np.array(-0.16163824, dtype=cp.float32)
+    data_max = np.array(2.7530956, dtype=cp.float32)
 
     #--- testing normalize_cupy  ---#
-    data_normalize_cupy = normalize_cupy(data, flats, darks, cutoff=10, minus_log=True)
-    for _ in range(10):
-        assert_allclose(cp.min(data_normalize_cupy), data_min, rtol=1e-06)
-        assert_allclose(cp.max(data_normalize_cupy), data_max, rtol=1e-06)
-
-    #: free up GPU memory by no longer referencing the variables
-    data_normalize_cupy = flats = darks = data_min = data_max = _data_1d = None
-    cp._default_memory_pool.free_all_blocks()
+    data_normalize = cp.asnumpy(normalize_cupy(data, flats, darks, cutoff=10, minus_log=True))
+    
+    assert_allclose(np.min(data_normalize), data_min, rtol=1e-06)
+    assert_allclose(np.max(data_normalize), data_max, rtol=1e-06)
