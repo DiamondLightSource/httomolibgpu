ecb6cb480581bfe7d48098b395e205f5999fe0e9
Team GPU <team-gpu@xcelerit.com>
2023-02-27 13:40:29 +0000

    tests refactoring

--- tests/test_recon/test_recon.py
+++ tests/test_recon/test_recon.py
@@ -1,10 +1,10 @@
 import time
 import cupy as cp
-import nvtx
+from cupy.cuda import nvtx
 import numpy as np
 import pytest
 from httomolib.prep.normalize import normalize_cupy
-from httomolib.recon.rotation import find_center_360, find_center_vo_cupy, find_center_360_cupy
+from httomolib.recon.rotation import find_center_360, find_center_vo_cupy
 from numpy.testing import assert_allclose
 
 
@@ -32,12 +32,10 @@ def test_find_center_vo_cupy_ones(ensure_clean_memory):
 
 
 def test_find_center_360_ones(gpu):
-    mat = np.ones(shape=(100, 100, 100))
-    if gpu:
-        (cor, overlap, side, overlap_position) = find_center_360_cupy(cp.asarray(mat))
-    else:
-        (cor, overlap, side, overlap_position) = find_center_360(mat)
+    xp = cp if gpu else np
+    mat = xp.ones(shape=(100, 100, 100), dtype=xp.float32)
     
+    (cor, overlap, side, overlap_position) = find_center_360(mat)
 
     assert_allclose(cor, 5.0)
     assert_allclose(overlap, 12.0)
@@ -48,10 +46,8 @@ def test_find_center_360_ones(gpu):
 
 def test_find_center_360_data(host_data, gpu):
     eps = 1e-5
-    if gpu:
-        (cor, overlap, side, overlap_pos) = find_center_360_cupy(cp.asarray(host_data))
-    else:
-        (cor, overlap, side, overlap_pos) = find_center_360(host_data)
+    data = cp.asarray(host_data) if gpu else host_data
+    (cor, overlap, side, overlap_pos) = find_center_360(data, norm=True, denoise=False)
     
     assert_allclose(cor, 132.45317, rtol=eps)
     assert_allclose(overlap, 53.093666, rtol=eps)
@@ -64,70 +60,60 @@ def test_find_center_360_data(host_data, gpu):
 
 
 @cp.testing.gpu
-def test_find_center_360_unity(ensure_clean_memory, have_gpu):
+@pytest.mark.parametrize("norm", [False, True], ids=["no_normalise", "normalise"])
+@pytest.mark.parametrize("overlap", [False, True], ids=["no_overlap", "overlap"])
+@pytest.mark.parametrize("denoise", [False, True], ids=["no_denoise", "denoise"])
+@pytest.mark.parametrize("side", [1, 0])
+@cp.testing.numpy_cupy_allclose(rtol=1e-5)
+def test_find_center_360_unity(ensure_clean_memory, xp, norm, overlap, denoise, side):
     eps = 1e-5
 
-    host_data = np.random.random_sample(size=(1801, 5, 2560)).astype(np.float32) * 2.0 + 0.001
-    data = cp.asarray(host_data)
+    # because it's random, we explicitly seed and use numpy only, to match the data
+    np.random.seed(12345)
+    data = np.random.random_sample(size=(128, 1, 512)).astype(np.float32) * 2.0 + 0.001
+    data = xp.asarray(data)
+    
+    (cor, overlap, side, overlap_pos) = find_center_360(data, use_overlap=overlap, 
+                                                        norm=norm, denoise=denoise, 
+                                                        side=side)
 
-    (cor, overlap, side, overlap_pos) = find_center_360(host_data)
-    (cor_gpu, overlap_gpu, side_gpu, overlap_pos_gpu) = find_center_360_cupy(data)
-
-    np.testing.assert_allclose(cor, cor_gpu, rtol=eps)
-    np.testing.assert_allclose(overlap, overlap_gpu, rtol=eps)
-    np.testing.assert_allclose(side, side_gpu, rtol=eps)
-    np.testing.assert_allclose(overlap_pos, overlap_pos_gpu, rtol=eps)
+    return xp.asarray([cor, overlap, side, overlap_pos])
+    
 
 
 @pytest.mark.perf
-def test_find_center_360_performance(gpu):
-    host_data = np.random.random_sample(size=(1801, 5, 2560)).astype(np.float32) * 2.0 + 0.001
+def test_find_center_360_performance(gpu, ensure_clean_memory):
+    xp = cp if gpu else np
+    data = xp.random.random_sample(size=(1801, 5, 2560)).astype(np.float32) * 2.0 + 0.001
 
-    if gpu:
-        data = cp.asarray(host_data)
-        # cold run
-        find_center_360_cupy(data)
+    # cold run
+    find_center_360(data, use_overlap=True, norm=True, denoise=True)
 
-        dev = cp.cuda.Device()
-        dev.synchronize()
+    dev = cp.cuda.Device()
+    dev.synchronize()
 
-        start = time.perf_counter_ns()
-        nvtx.RangePush("Core")
-        for _ in range(10):
-            # have to take copy, as data is modified in-place
-            find_center_360_cupy(data)
-        nvtx.RangePop()
-        dev.synchronize()
-        
-        duration_ms = float(time.perf_counter_ns() - start) * 1e-6 / 10
-    else:
-        # cold run
-        find_center_360(host_data)
-
-        start = time.perf_counter_ns()
-        for _ in range(10):
-            # have to take copy, as data is modified in-place
-            find_center_360(host_data)
-        
-        duration_ms = float(time.perf_counter_ns() - start) * 1e-6 / 10
+    start = time.perf_counter_ns()
+    nvtx.RangePush("Core")
+    for _ in range(10):
+        # have to take copy, as data is modified in-place
+        find_center_360(data, use_overlap=True, norm=True, denoise=True)
+    nvtx.RangePop()
+    dev.synchronize()
     
+    duration_ms = float(time.perf_counter_ns() - start) * 1e-6 / 10
     
     assert "performance in ms" == duration_ms
 
 
 
 def test_find_center_360_1D_raises(host_data, gpu):
+    data = cp.asarray(host_data) if gpu else host_data
+    xp = cp if gpu else np
+    
     #: 360-degree sinogram must be a 3d array
-
     with pytest.raises(ValueError):
-        if gpu:
-            find_center_360_cupy(cp.asarray(host_data[:, 10, :]))
-        else:
-            find_center_360(host_data[:, 10, :])
+        find_center_360(data[:, 10, :])
     
     with pytest.raises(ValueError):
-        if gpu:
-            find_center_360_cupy(cp.ones(10))
-        else:
-            find_center_360(np.ones(10))
+        find_center_360(xp.ones(10))
         
