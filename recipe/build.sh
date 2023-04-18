94bb5340792d0bb737bc6dadfbfeb193d10c6a03
Daniil Kazantsev <dkazanc@hotmail.com>
2023-04-18 20:10:02 +0100

    with manifest file, the cuda kernels now copied

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-python -m pip install . --ignore-installed -vvv
+set -xe 
+cp -rv "$RECIPE_DIR/../httomolib/cuda_kernels/" "$SRC_DIR/httomolib"
+
+cd $SRC_DIR
+
+python -m pip install .
+
