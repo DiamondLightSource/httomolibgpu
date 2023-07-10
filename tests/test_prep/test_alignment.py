12d683258b6f78391afced3102d6826c8579bcbd
Naman Gera <namangera15@gmail.com>
2023-07-10 13:51:26 +0100

    Make distortion correction method work only for 3d data

--- tests/test_prep/test_alignment.py
+++ tests/test_prep/test_alignment.py
@@ -16,11 +16,11 @@ from tests import MaxMemoryHook
 
 @cp.testing.gpu
 @pytest.mark.parametrize(
-    "image, max_value, mean_value",
+    "image, mean_value, mean_sum, sum",
     [
-        ("dot_pattern_03.tif", 255, 200.16733869461675),
-        ("peppers.tif", 228, 95.51871109008789),
-        ("cameraman.tif", 254, 122.2400016784668),
+        ("dot_pattern_03.tif", 200.16733869461675, 20016.733869461674, 214366729300),
+        ("peppers.tif", 95.51871109008789, 9551.871109008789, 2503965700),
+        ("cameraman.tif", 122.2400016784668, 12224.00016784668, 3204448300),
     ],
     ids=["dot_pattern_03", "peppers", "cameraman"],
 )
@@ -34,24 +34,42 @@ def test_correct_distortion(
     ensure_clean_memory,
     image,
     implementation,
-    max_value,
     mean_value,
+    mean_sum,
+    sum,
 ):
     distortion_coeffs_path = os.path.join(
         distortion_correction_path, "distortion-coeffs.txt"
     )
 
     path = os.path.join(distortion_correction_path, image)
-    im_host = imread(path)
-    im = cp.asarray(im_host)
+    im_host = np.asarray(imread(path))
+    im_host = np.expand_dims(im_host, axis=0)
+    # replicate into a stack of images
+    im_stack = cp.asarray(cp.tile(im_host, (100, 1, 1)))
 
-    preview = {"starts": [0, 0], "stops": [im.shape[0], im.shape[1]], "steps": [1, 1]}
-    corrected_data = implementation(im, distortion_coeffs_path, preview).get()
-    
+    preview = {
+        "starts": [0, 0],
+        "stops": [im_stack.shape[0], im_stack.shape[1]],
+        "steps": [1, 1],
+    }
+    corrected_data = implementation(im_stack, distortion_coeffs_path, preview).get()
+
     assert_allclose(np.mean(corrected_data), mean_value)
-    assert np.max(corrected_data) == max_value
+    assert_allclose(np.mean(corrected_data, axis=(1, 2)).sum(), mean_sum, rtol=1e-6)
+    assert np.sum(corrected_data) == sum
 
     assert corrected_data.dtype == np.uint8
+    assert corrected_data.ndim == 3
+
+
+def test_correct_distortion_1d_raises(distortion_correction_path, ensure_clean_memory):
+    with pytest.raises(ValueError):
+        preview = {"starts": [0, 0], "stops": [1, 1], "steps": [1, 1]}
+        distortion_coeffs_path = os.path.join(
+            distortion_correction_path, "distortion-coeffs.txt"
+        )
+        distortion_correction_proj_discorpy(cp.ones(1), distortion_coeffs_path, preview)
 
 
 @pytest.mark.parametrize("stack_size", [20, 50, 100])
@@ -60,7 +78,9 @@ def test_correct_distortion(
     [distortion_correction_proj, distortion_correction_proj_discorpy],
     ids=["cupy", "tomopy"],
 )
-def test_distortion_correction_meta(distortion_correction_path, stack_size, implementation):
+def test_distortion_correction_meta(
+    distortion_correction_path, stack_size, implementation
+):
     distortion_coeffs_path = os.path.join(
         distortion_correction_path, "distortion-coeffs.txt"
     )
@@ -70,20 +90,31 @@ def test_distortion_correction_meta(distortion_correction_path, stack_size, impl
     im_host = np.expand_dims(im_host, axis=0)
     # replicate into a stack of images
     im_stack = cp.asarray(np.tile(im_host, (stack_size, 1, 1)))
+
     hook = MaxMemoryHook(im_stack.size * im_stack.itemsize)
-    preview = {"starts": [0, 0], "stops": [im_stack.shape[1], im_stack.shape[2]], "steps": [1, 1]}
-    
+    preview = {
+        "starts": [0, 0],
+        "stops": [im_stack.shape[1], im_stack.shape[2]],
+        "steps": [1, 1],
+    }
+
     with hook:
         implementation(im_stack, distortion_coeffs_path, preview)
-    
+
     # make sure estimator function is within range (80% min, 100% max)
     max_mem = hook.max_mem
     actual_slices = im_stack.shape[0]
     estimated_slices, dtype_out, output_dims = implementation.meta.calc_max_slices(
-        0, (im_stack.shape[1], im_stack.shape[2]),
-        im_stack.dtype, max_mem, 
-        metadata_path=distortion_coeffs_path, preview=preview)
+        0,
+        (im_stack.shape[1], im_stack.shape[2]),
+        im_stack.dtype,
+        max_mem,
+        metadata_path=distortion_coeffs_path,
+        preview=preview,
+    )
     assert estimated_slices <= actual_slices
-    assert estimated_slices / actual_slices >= 0.8 
-    
-    assert implementation.__name__ in method_registry['httomolibgpu']['prep']['alignment']
+    assert estimated_slices / actual_slices >= 0.8
+
+    assert (
+        implementation.__name__ in method_registry["httomolibgpu"]["prep"]["alignment"]
+    )
