05e366217e7977f56a2d6bb6e5e396617784e99a
dkazanc <dkazanc@hotmail.com>
2023-09-21 16:36:48 +0100

    test normalise processed

--- tests/test_prep/test_normalize.py
+++ tests/test_prep/test_normalize.py
@@ -7,10 +7,8 @@ import pytest
 from numpy.testing import assert_allclose, assert_equal
 from httomolibgpu.prep.normalize import normalize
 from httomolibgpu import method_registry
+
 from cupy.cuda import nvtx
-
-from tests import MaxMemoryHook
-
 
 @cp.testing.gpu
 def test_normalize_1D_raises(data, flats, darks, ensure_clean_memory):
@@ -36,26 +34,6 @@ def test_normalize(data, flats, darks, ensure_clean_memory):
     assert_allclose(np.median(data_normalize), 0.01723744, rtol=1e-06)
     assert_allclose(np.std(data_normalize), 0.524382, rtol=1e-06)
 
-
-@cp.testing.gpu
-def test_normalize_meta(data, flats, darks, ensure_clean_memory):
-    # --- testing normalize  ---#
-    hook = MaxMemoryHook()
-    with hook:
-        data_normalize = normalize(cp.copy(data), flats, darks, minus_log=True).get()
-
-    # make sure estimator function is within range (80% min, 100% max)
-    max_mem = hook.max_mem
-    actual_slices = data.shape[0]
-    estimated_slices, dtype_out, output_dims = normalize.meta.calc_max_slices(0,
-                                                         (data.shape[1], data.shape[2]),                                                         
-                                                         data.dtype, max_mem,
-                                                         flats=flats, darks=darks)
-    assert estimated_slices <= actual_slices
-    assert estimated_slices / actual_slices >= 0.8
-    assert normalize.meta.pattern == 'projection'
-    assert 'normalize' in method_registry['httomolibgpu']['prep']['normalize']
-    
 
 @cp.testing.gpu
 @pytest.mark.perf
