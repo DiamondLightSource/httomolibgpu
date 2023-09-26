fixes rotation

tests/test_recon/test_rotation.py
@@ -25,8 +25,6 @@ def test_find_center_vo(data, flats, darks):
 
     #: Check that we only get a float32 output
     assert cor.dtype == np.float32
-    assert find_center_vo.meta.pattern == 'sinogram'
-    assert 'find_center_vo' in method_registry['httomolibgpu']['recon']['rotation']
 
 
 @cp.testing.gpu
@@ -38,17 +36,13 @@ def test_find_center_vo_ones(ensure_clean_memory):
     mat = None  #: free up GPU memory
 
 
-
 @cp.testing.gpu
 def test_find_center_vo_random(ensure_clean_memory):
     np.random.seed(12345)
-    data_host = np.random.random_sample(size=(1801, 5, 2560)).astype(np.float32) * 2.0
+    data_host = np.random.random_sample(size=(900, 1, 1280)).astype(np.float32) * 2.0
     data = cp.asarray(data_host, dtype=np.float32)
-
     cent = find_center_vo(data)
-
-    assert_allclose(cent, 1246.75)
-
+    assert_allclose(cent, 680.75)    
 
 @cp.testing.gpu
 def test_find_center_vo_calculate_chunks():
@@ -77,19 +71,6 @@ def test_find_center_vo_calculate_chunks():
             assert diffs[0] > 0
             np.testing.assert_array_equal(diffs[:-1], diffs[0])
             assert diffs[-1] <= diffs[0]
-
-
-@cp.testing.gpu
-def test_find_center_vo_data_chunked(data, flats, darks):
-    data = normalize(data, flats, darks)
-
-    # we emulate less memory here - with this amount, we get 12 chunks and 3 chunks in the 2 calls,
-    # and the data should be the same as when it's all fitting
-    with mock.patch('httomolibgpu.recon.rotation._get_available_gpu_memory', return_value=10000000):
-        cor = find_center_vo(data)
-
-    assert_allclose(cor, 79.5)
-
 
 @cp.testing.gpu
 @pytest.mark.perf
@@ -133,11 +114,6 @@ def test_find_center_360_data(data):
     assert_allclose(overlap, 53.093666, rtol=eps)
     assert side == 1
     assert_allclose(overlap_pos, 111.906334, rtol=eps)
-
-    #: Check meta
-    assert find_center_360.meta.pattern == 'sinogram'
-    assert 'find_center_360' in method_registry['httomolibgpu']['recon']['rotation']
-
 
 @cp.testing.gpu
 def test_find_center_360_1D_raises(data):