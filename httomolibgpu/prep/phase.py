c0075e9a6a882c24fc6070dfe35b3812bada014e
dkazanc <dkazanc@hotmail.com>
2024-10-21 17:20:24 +0100

    moving the padding estimator into a separate function

--- httomolibgpu/prep/phase.py
+++ httomolibgpu/prep/phase.py
@@ -348,6 +348,35 @@ def _shift_bit_length(x: int) -> int:
     return 1 << (x - 1).bit_length()
 
 
+def _calculate_pad_size(datashape: tuple) -> list:
+    """Calculating the padding size
+
+    Args:
+        datashape (tuple): the shape of the 3D data
+
+    Returns:
+        list: the padded dimensions
+    """
+    pad_list = []
+    for index, element in enumerate(datashape):
+        if index == 0:
+            pad_width = (0, 0)  # do not pad the slicing dim
+        else:
+            diff = _shift_bit_length(element + 1) - element
+            if element % 2 == 0:
+                pad_width_scalar = diff // 2
+                pad_width = (pad_width_scalar, pad_width_scalar)
+            else:
+                # need an uneven padding for odd-number lengths
+                left_pad = diff // 2
+                right_pad = diff - left_pad
+                pad_width = (left_pad, right_pad)
+
+        pad_list.append(pad_width)
+
+    return pad_list
+
+
 def _pad_projections_to_second_power(tomo: cp.ndarray) -> Union[cp.ndarray, tuple]:
     """
     Performs padding of each projection to the next power of 2.
@@ -365,26 +394,11 @@ def _pad_projections_to_second_power(tomo: cp.ndarray) -> Union[cp.ndarray, tupl
     """
     full_shape_tomo = cp.shape(tomo)
 
-    pad_tup = []
-    for index, element in enumerate(full_shape_tomo):
-        if index == 0:
-            pad_width = (0, 0)  # do not pad the slicing dim
-        else:
-            diff = _shift_bit_length(element + 1) - element
-            if element % 2 == 0:
-                pad_width_scalar = diff // 2
-                pad_width = (pad_width_scalar, pad_width_scalar)
-            else:
-                # need an uneven padding for odd-number lengths
-                left_pad = diff // 2
-                right_pad = diff - left_pad
-                pad_width = (left_pad, right_pad)
+    pad_list = _calculate_pad_size(full_shape_tomo)
 
-        pad_tup.append(pad_width)
+    padded_tomo = cp.pad(tomo, tuple(pad_list), "edge")
 
-    padded_tomo = cp.pad(tomo, tuple(pad_tup), "edge")
-
-    return padded_tomo, pad_tup
+    return padded_tomo, tuple(pad_list)
 
 
 def _paganin_filter_factor2(energy, dist, alpha, w2):
