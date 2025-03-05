0b3cd772c1501d41d9d08030be4324c39caa7d14
dkazanc <dkazanc@hotmail.com>
2025-03-05 12:02:21 +0000

    api modifications for lprec and zenodo test

--- httomolibgpu/recon/algorithm.py
+++ httomolibgpu/recon/algorithm.py
@@ -112,6 +112,8 @@ def LPRec(
     data: cp.ndarray,
     angles: np.ndarray,
     center: Optional[float] = None,
+    filter_type: str = 'shepp',
+    cutoff_freq: float = 1.0,
     recon_size: Optional[int] = None,
     recon_mask_radius: Optional[float] = 0.95,
     neglog: bool = False,
@@ -129,6 +131,10 @@ def LPRec(
         An array of angles given in radians.
     center : float, optional
         The center of rotation (CoR).
+    filter_type : str
+        Filter type, the accepted strings are: none, ramp, shepp, cosine, cosine2, hamming, hann, parzen.
+    cutoff_freq : float
+        Cutoff frequency parameter for a filter. The higher values increase resolution and but also noise.
     recon_size : int, optional
         The [recon_size, recon_size] shape of the reconstructed slice in pixels.
         By default (None), the reconstructed size will be the dimension of the horizontal detector.
@@ -151,6 +157,8 @@ def LPRec(
         _take_neg_log(data) if neglog else data,
         recon_mask_radius=recon_mask_radius,
         data_axes_labels_order=input_data_axis_labels,
+        filter_type = filter_type,
+        cutoff_freq = cutoff_freq,
     )
     cp._default_memory_pool.free_all_blocks()
     return cp.require(cp.swapaxes(reconstruction, 0, 1), requirements="C")
--- zenodo-tests/test_recon/test_algorithm.py
+++ zenodo-tests/test_recon/test_algorithm.py
@@ -43,6 +43,33 @@ def test_reconstruct_FBP_i12_dataset1(i12_dataset1):
     assert recon_data.shape == (2560, 50, 2560)
 
 
+def test_reconstruct_LPRec_i12_dataset1(i12_dataset1):
+    force_clean_gpu_memory()
+    projdata = i12_dataset1[0]
+    angles = i12_dataset1[1]
+    flats = i12_dataset1[2]
+    darks = i12_dataset1[3]
+    del i12_dataset1
+
+    data_normalised = normalize(projdata, flats, darks, minus_log=True)
+    data_normalised_cut = data_normalised[:,5:8,:]
+    del flats, darks, projdata, data_normalised
+    force_clean_gpu_memory()
+
+    recon_data = LPRec(
+        data_normalised_cut,
+        np.deg2rad(angles),
+        center=1253.75,
+        filter_type='shepp',
+        cutoff_freq = 1.0,
+    )
+    assert recon_data.flags.c_contiguous
+    recon_data = recon_data.get()
+    assert_allclose(np.sum(recon_data), 46569.39, rtol=1e-07, atol=1e-6)
+    assert recon_data.dtype == np.float32
+    assert recon_data.shape == (2560, 3, 2560)
+
+
 def test_reconstruct_LP_REC_i13_dataset1(i13_dataset1):
     force_clean_gpu_memory()
     projdata = i13_dataset1[0]
