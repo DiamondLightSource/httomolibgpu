0b567077dca5a70f45a4826d3ba4ec7289fd9f5a
dkazanc <dkazanc@hotmail.com>
2025-11-13 11:05:47 +0000

    correcting paganin tests and adding zenodo test

--- tests/test_prep/test_phase.py
+++ tests/test_prep/test_phase.py
@@ -4,55 +4,53 @@ import cupy as cp
 import numpy as np
 import pytest
 from cupy.cuda import nvtx
-from httomolibgpu.prep.phase import paganin_filter_tomopy
+from httomolibgpu.prep.phase import paganin_filter
 from numpy.testing import assert_allclose
 
 eps = 1e-6
 
 
-# paganin filter tomopy
-def test_paganin_filter_tomopy_1D_raises(ensure_clean_memory):
+# paganin filter
+def test_paganin_filter_1D_raises(ensure_clean_memory):
     _data = cp.ones(10)
     with pytest.raises(ValueError):
-        paganin_filter_tomopy(_data)
+        paganin_filter(_data)
 
     _data = None  #: free up GPU memory
 
 
-def test_paganin_filter_tomopy(data):
-    # --- testing the Paganin filter from TomoPy on tomo_standard ---#
-    filtered_data = paganin_filter_tomopy(data).get()
+def test_paganin_filter(data):
+    # --- testing the Paganin filter on tomo_standard ---#
+    filtered_data = paganin_filter(data).get()
 
     assert filtered_data.ndim == 3
-    assert_allclose(np.mean(filtered_data), -6.74213, rtol=eps)
-    assert_allclose(np.max(filtered_data), -6.496699, rtol=eps)
+    assert_allclose(np.mean(filtered_data), -6.725061, rtol=eps)
+    assert_allclose(np.max(filtered_data), -6.367116, rtol=eps)
 
     #: make sure the output is float32
     assert filtered_data.dtype == np.float32
     assert filtered_data.flags.c_contiguous
 
 
-def test_paganin_filter_tomopy_energy100(data):
-    filtered_data = paganin_filter_tomopy(data, energy=100.0).get()
+def test_paganin_filter_energy100(data):
+    filtered_data = paganin_filter(data, energy=100.0).get()
 
-    assert_allclose(np.mean(filtered_data), -6.73455, rtol=1e-05)
-    assert_allclose(np.min(filtered_data), -6.909582, rtol=eps)
+    assert_allclose(np.mean(filtered_data), -6.7109337, rtol=1e-05)
+    assert_allclose(np.min(filtered_data), -6.9103956, rtol=eps)
 
     assert filtered_data.ndim == 3
     assert filtered_data.dtype == np.float32
 
 
-def test_paganin_filter_tomopy_dist75(data):
-    filtered_data = paganin_filter_tomopy(data, dist=75.0, alpha=1e-6).get()
+def test_paganin_filter_dist3(data):
+    filtered_data = paganin_filter(data, distance=3.0, ratio_delta_beta=500).get()
 
-    assert_allclose(np.sum(np.mean(filtered_data, axis=(1, 2))), -1215.4985, rtol=1e-6)
-    assert_allclose(np.sum(filtered_data), -24893412.0, rtol=1e-6)
-    assert_allclose(np.mean(filtered_data[0, 60:63, 90]), -6.645878, rtol=1e-6)
-    assert_allclose(np.sum(filtered_data[50:100, 40, 1]), -343.5908, rtol=1e-6)
+    assert_allclose(np.sum(np.mean(filtered_data, axis=(1, 2))), -1214.3943, rtol=1e-6)
+    assert_allclose(np.sum(filtered_data), -24870786.0, rtol=1e-6)    
 
 
 @pytest.mark.perf
-def test_paganin_filter_tomopy_performance(ensure_clean_memory):
+def test_paganin_filter_performance(ensure_clean_memory):
     # Note: low/high and size values taken from sample2_medium.yaml real run
 
     # this test needs ~20GB of memory with 1801 - we'll divide depending on GPU memory
@@ -69,14 +67,14 @@ def test_paganin_filter_tomopy_performance(ensure_clean_memory):
 
     # run code and time it
     # cold run first
-    paganin_filter_tomopy(data)
+    paganin_filter(data)
     dev = cp.cuda.Device()
     dev.synchronize()
 
     start = time.perf_counter_ns()
     nvtx.RangePush("Core")
     for _ in range(10):
-        paganin_filter_tomopy(data)
+        paganin_filter(data)
 
     nvtx.RangePop()
     dev.synchronize()
