f83d5a1458b2f9650f691f1af64d71043bc0957a
Lőrinc Serfőző <lorinc.serfozo@radwaytech.com>
2025-12-16 15:43:47 +0100

    Paganin padding zenodo test cases

--- zenodo-tests/test_prep/test_phase.py
+++ zenodo-tests/test_prep/test_phase.py
@@ -8,7 +8,12 @@ from conftest import force_clean_gpu_memory
 
 # ----------------------------------------------------------#
 # appplying paganin filter to i12_dataset3
-def test_paganin_filter_i12_dataset3(i12_dataset3, ensure_clean_memory):
+@pytest.mark.parametrize("test_case", [
+    ('next_power_of_2', -6.2188172, -10.92260456),
+    ('next_fast_length', -6.867182, -10.92272),
+    (80, -6.32930, -10.92270),
+])
+def test_paganin_filter_i12_dataset3(i12_dataset3, test_case, ensure_clean_memory):
     inputdata = cp.empty((3, 2050, 2560))
     inputdata[0, :, :] = i12_dataset3[0]
     inputdata[1, :, :] = i12_dataset3[1]
@@ -17,7 +22,7 @@ def test_paganin_filter_i12_dataset3(i12_dataset3, ensure_clean_memory):
 
     ensure_clean_memory
 
-    filtered_paganin = paganin_filter(inputdata)
+    filtered_paganin = paganin_filter(inputdata, padding_method=test_case[0])
 
-    assert pytest.approx(np.max(cp.asnumpy(filtered_paganin)), rel=1e-3) == -6.2188172
-    assert pytest.approx(np.min(cp.asnumpy(filtered_paganin)), rel=1e-3) == -10.92260456
+    assert pytest.approx(np.max(cp.asnumpy(filtered_paganin)), rel=1e-3) == test_case[1]
+    assert pytest.approx(np.min(cp.asnumpy(filtered_paganin)), rel=1e-3) == test_case[2]
