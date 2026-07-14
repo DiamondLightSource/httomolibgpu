a1131e00af183ad8da7d0e463c894634ca8c5225
algol <dkazanc@hotmail.com>
2026-07-14 12:00:23 +0100

    distortion correction explicit coefficients

--- tests/test_prep/test_alignment.py
+++ tests/test_prep/test_alignment.py
@@ -24,7 +24,7 @@ from numpy.testing import assert_allclose
     [distortion_correction_proj_discorpy],
     ids=["tomopy"],
 )
-def test_correct_distortion(
+def test_correct_distortion_file(
     distortion_correction_path,
     ensure_clean_memory,
     image,
@@ -43,7 +43,65 @@ def test_correct_distortion(
     shift_xy = [0, 0]
     step_xy = [1, 1]
     corrected_data = implementation(
-        im, distortion_coeffs_path, shift_xy, step_xy, order=1, mode="reflect"
+        im,
+        distortion_coeffs_path,
+        None,
+        None,
+        None,
+        shift_xy,
+        step_xy,
+        order=1,
+        mode="reflect",
+    ).get()
+
+    assert_allclose(np.mean(corrected_data), mean_value)
+    assert np.max(corrected_data) == max_value
+    assert corrected_data.dtype == cp.uint16
+    assert corrected_data.flags.c_contiguous
+
+
+@pytest.mark.parametrize(
+    "image, max_value, mean_value",
+    [
+        ("dot_pattern_03.tif", 255, 200.16733869461675),
+        ("peppers.tif", 228, 95.51871109008789),
+        ("cameraman.tif", 254, 122.2400016784668),
+    ],
+    ids=["dot_pattern_03", "peppers", "cameraman"],
+)
+@pytest.mark.parametrize(
+    "implementation",
+    [distortion_correction_proj_discorpy],
+    ids=["tomopy"],
+)
+def test_correct_distortion_parameters(
+    distortion_correction_path,
+    ensure_clean_memory,
+    image,
+    implementation,
+    max_value,
+    mean_value,
+):
+    distortion_coeffs_path = os.path.join(
+        distortion_correction_path, "distortion-coeffs.txt"
+    )
+
+    path = os.path.join(distortion_correction_path, image)
+    im_host = imread(path)
+    im = cp.asarray(im_host.astype(cp.uint16))
+
+    shift_xy = [0, 0]
+    step_xy = [1, 1]
+    corrected_data = implementation(
+        im,
+        None,
+        1907.252211174307,
+        1522.9526884119646,
+        [1.0003402629927707, 5.332383469534285e-10, 3.426741168508618e-09],
+        shift_xy,
+        step_xy,
+        order=1,
+        mode="reflect",
     ).get()
 
     assert_allclose(np.mean(corrected_data), mean_value)
